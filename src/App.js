837f56a65f74e9659d5836ccc8e90b8308f2eb48
sharma.59 <sharma.59@iitjac.in>
2021-04-21 11:48:51 +0530

    for cicd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [num , setnum] = useState(0);
+  const increase = (d)=>{
+    setnum(num+100);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is a dummy application for ci/cd pipeline</h1>
+      <button type = "button" className = "btn btn-danger my-5" onClick = {increase}>Click Me</button>
+      <h2>{num}</h2>
     </div>
   );
 }
-
+//what to do at which step will help us know what to do at what step
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+test('checking 0',()=>{
+  expect(0).toEqual(0);
 });
